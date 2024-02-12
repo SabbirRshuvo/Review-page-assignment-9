5d59179b721982c43d4afd2ab0f1976d9bdd52d0
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-02-12 12:45:51 +0600

    home page complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import './Home.css'
+import abc from './phone.jpg'
 const Home = () => {
     return (
-        <div>
-            <h2>This is home page</h2>
+        <div className='container'>
+            <div className="photo-section">
+                <div className="text-section">
+                    <span className='first'>Your First Phone</span>
+                    <br />
+                    <span className='second'>Your best phone</span>
+                    <p className='paragraph'>A mobile phone is a personal communication device used to make and receive calls, send and receive messages, access the internet and perform other tasks via a wireless connection</p>
+                    <button className='btn'>Live Demo</button>
+                </div>
+                <div className="img-section">
+                    <img src={abc} alt="" />
+                </div>
+            </div>
+            <div className="footer-section"></div>
         </div>
     );
 };
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const NotFound = () => {
+    alert('this page is not found')
     return (
         <div>
             <h1>404</h1>
+            <h2>This page is not found</h2>
         </div>
     );
 };

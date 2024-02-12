509bd9bca6fdc52035b835150c0efdd2b6a7affd
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-02-12 12:12:33 +0600

    header hr line

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='body'>
             <nav>
                 <Link to='/home'>Home</Link>
                 <Link to='/reviews'>Reviews</Link>
